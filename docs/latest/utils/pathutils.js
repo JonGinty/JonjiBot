7fbb281a93dea23fbb6a870e5889dae4ecd46d3f
Jon Ginty <j.ginty@outlook.com>
2021-09-15 20:27:38 +0100

    basic error handling in extremely flaky url parsing function

--- docs/latest/utils/pathutils.js
+++ docs/latest/utils/pathutils.js
@@ -1,7 +1,12 @@
 getAllUrlParameters = function() {
-    var search = location.search.substring(1);
-    if (!search) return {};
-    return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+    try {
+        var search = location.search.substring(1);
+        if (!search) return {};
+        return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+    } catch (e) {
+        console.error(e);
+    }
+    return {};
 }
 
 buildHelp = function() {
