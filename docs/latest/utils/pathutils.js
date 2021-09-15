f72eeec083a8d51f6c5c246ca4bbb68a2fd8f33c
Jon Ginty <j.ginty@outlook.com>
2021-09-15 20:25:02 +0100

    Basic twitch and youtube social options

---
+++ docs/latest/utils/pathutils.js
@@ -0,0 +1,9 @@
+getAllUrlParameters = function() {
+    var search = location.search.substring(1);
+    if (!search) return {};
+    return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+}
+
+buildHelp = function() {
+    
+}
