4b15ff2fefa1ab640ff39c9a702c2d18f3ef4370
Jon Ginty <j.ginty@outlook.com>
2021-09-16 19:25:51 +0100

    Publish typescript build as experimental

---
+++ docs/experimental/social-overlays/socialOverlays.js
@@ -0,0 +1,54 @@
+import { getAllUrlParameters } from "../lib/pathutils.js";
+export function init() {
+    const socials = [
+        {
+            type: "youtube",
+            url: "youtube.com/",
+            colour: "#f93232"
+        },
+        {
+            type: "twitch",
+            url: "twitch.tv/",
+            colour: "#6441a5"
+        },
+        {
+            type: "facebook",
+            url: "",
+            colour: "#4267B2",
+            icon: "facebook-square"
+        },
+        {
+            type: "twitter",
+            url: "twitter.com/",
+            colour: "#1DA1F2",
+            icon: "twitter-square"
+        },
+        {
+            type: "instagram",
+            url: "",
+            colour: "#F77737",
+        },
+    ];
+    const safeChange = (logic, el) => {
+        if (!el)
+            return;
+        logic(el);
+    };
+    const args = getAllUrlParameters();
+    const showHelp = () => {
+        safeChange(el => el.style.display = "none", document.querySelector(".url-ish"));
+        safeChange(el => el.style.display = "", document.querySelector(".help-text"));
+    };
+    if (!args.type || !args.name) {
+        showHelp();
+        return;
+    }
+    const social = socials.find(s => s.type === args.type);
+    if (!social) {
+        showHelp();
+        return;
+    }
+    safeChange(el => el.classList.add("fa-" + (social.icon || args.type)), document.querySelector("#social-icon"));
+    safeChange(el => el.innerText = (args.showurl ? social.url : "") + args.name, document.querySelector("#social-name"));
+    safeChange(el => el.style.color = social.colour, document.querySelector(".url-ish"));
+}
